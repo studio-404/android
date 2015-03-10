fe9dacccd21eb354f08a42a658ae895951e85c3f
gio <hosting@404.ge>
2015-03-10 15:16:41 +0400

    change button and textfield width

--- app/src/main/java/ge/studio404/allison/MainActivity.java
+++ app/src/main/java/ge/studio404/allison/MainActivity.java
@@ -8,6 +8,10 @@ import android.widget.RelativeLayout;
 import android.widget.Button;
 import android.graphics.Color;
 import android.widget.EditText;
+import android.content.res.Resources;
+import android.util.TypedValue;
+
+import java.lang.reflect.Type;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -17,25 +21,35 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         //Layout
         RelativeLayout giosLayout = new RelativeLayout(this);
-        giosLayout.setBackgroundColor(Color.parseColor("#2b2b2b"));
+        giosLayout.setBackgroundColor(Color.parseColor("#3099c6"));
 
         /*
         * Button START
         * */
             Button redButton = new Button(this);
             redButton.setText("Log in");
-            redButton.setBackgroundColor(Color.parseColor("#3099c6"));
+            redButton.setBackgroundColor(Color.parseColor("#2b2b2b"));
             redButton.setTextColor(Color.parseColor("#ffffff"));
+
             // set id
             redButton.setId(1);
+
             // set button Details
             RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
                     RelativeLayout.LayoutParams.WRAP_CONTENT,
                     RelativeLayout.LayoutParams.WRAP_CONTENT
             );
+
             // give rules to position widgits
             buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
             buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+            // get resources to make dp to px and set size of username field
+            Resources r1 = getResources();
+            int px1 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 120,
+                    r1.getDisplayMetrics());
+            redButton.setWidth(px1);
+
             //add wigests to layout
             giosLayout.addView(redButton, buttonDetails);
         /*
@@ -47,8 +61,10 @@ public class MainActivity extends ActionBarActivity {
         * User START
         * */
             EditText username = new EditText(this);
+
             // set ids
             username.setId(2);
+
             // set username Details
             RelativeLayout.LayoutParams usernameDeteils = new RelativeLayout.LayoutParams(
                     RelativeLayout.LayoutParams.WRAP_CONTENT,
@@ -58,6 +74,13 @@ public class MainActivity extends ActionBarActivity {
             usernameDeteils.addRule(RelativeLayout.ABOVE, redButton.getId());
             usernameDeteils.addRule(RelativeLayout.CENTER_HORIZONTAL);
             usernameDeteils.setMargins(0,0,0,50);
+
+            // get resources to make dp to px and set size of username field
+            Resources r2 = getResources();
+            int px2 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 200,
+                r2.getDisplayMetrics());
+            username.setWidth(px2);
+
             // add widget to layout
             giosLayout.addView(username, usernameDeteils);
          /*
